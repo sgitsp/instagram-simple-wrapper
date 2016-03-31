075bdf5bda40dd2aa00dcddf038b441d4db7fe36
Sigit Purwadi <sgt2426@gmail.com>
2016-04-01 04:52:01 +0700

    update link and image alt

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ $(function() {
 		limit: 16,
 		sortBy: 'most-recent',
 		resolution: 'standard_resolution',
-		template: '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3"><div class="photo-box"><div class="image-wrap"><a href="{{link}}"><img src="{{image}}"></a><div class="likes">{{likes}} Likes</div></div><div class="description">{{caption}}<div class="date">{{model.date}}</div></div></div></div>'
+		template: '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3"><div class="photo-box"><div class="image-wrap"><a href="{{link}}" target="_blank" alt="{{caption}}"><img src="{{image}}"></a><div class="likes">{{likes}} ❤ Likes</div></div><div class="description">{{caption}}<div class="date">{{model.date}}</div></div></div></div>'
 	});
 	feed.run();
 });
