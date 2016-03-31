f8025b4e919247fe3468d1ea78715bca80569e0c
Sigit Purwadi <sgt2426@gmail.com>
2016-04-01 04:34:27 +0700

    update photo limit to 16

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ $(function() {
 		accessToken: '577210186.8046de1.b57e8fd1fcbf4ee3a8d95bde49b5f151',
 		useHttp: true,
 		links: true,
-		limit: 8,
+		limit: 16,
 		sortBy: 'most-recent',
 		resolution: 'standard_resolution',
 		template: '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3"><div class="photo-box"><div class="image-wrap"><a href="{{link}}"><img src="{{image}}"></a><div class="likes">{{likes}} Likes</div></div><div class="description">{{caption}}<div class="date">{{model.date}}</div></div></div></div>'
